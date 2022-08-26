7b548e62a85a0847e9ef0688e5e113c42a2889e9
Mrugalla <florian.mrugalla@web.de>
2022-08-26 21:46:38 +0200

    added everything that doesn't break i think

--- Source/gui/MIDICCMonitor.h
+++ Source/gui/MIDICCMonitor.h
@@ -10,47 +10,19 @@ namespace gui
 	{
 		using Learn = audio::MIDILearn;
 
-		MIDICCMonitor(Utils& u, const Learn& _learn) :
-			Comp(u, "Monitors your input controller number for MIDI Learn.", CursorType::Default),
-			learn(_learn),
-			idx(learn.ccIdx.load()),
-			label(u, idx < 0 ? "cc: .." : toString())
-		{
-			addAndMakeVisible(label);
-			label.textCID = ColourID::Hover;
-			label.just = Just::left;
-			startTimerHz(24);
-		}
+		MIDICCMonitor(Utils&, const Learn&);
+		
 	protected:
 		const Learn& learn;
 		int idx;
 		Label label;
 
-		void paint(Graphics&) {}
+		void paint(Graphics&);
 
-		void resized() override
-		{
-			label.setBounds(getLocalBounds());
-		}
+		void resized() override;
 
-		void timerCallback() override
-		{
-			const auto nIdx = learn.ccIdx.load();
-			if (nIdx < 0)
-				return;
+		void timerCallback() override;
 
-			if (idx != nIdx)
-			{
-				idx = nIdx;
-				
-				label.setText(toString());
-				label.repaint();
-			}
-		}
-
-		String toString()
-		{
-			return "cc: " + String(idx);
-		}
+		String toString();
 	};
 }
