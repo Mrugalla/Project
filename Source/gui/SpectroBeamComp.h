775b99549d46b151e43c1252ac46f060bc7701ec
Mrugalla <florian.mrugalla@web.de>
2022-11-03 16:01:28 +0100

    CPPd spectroBeamComp

--- Source/gui/SpectroBeamComp.h
+++ Source/gui/SpectroBeamComp.h
@@ -19,58 +19,11 @@ namespace gui
 		static constexpr float SizeInv = 1.f / SizeF;
 		static constexpr float SizeFHalf = SizeF * .5f;
 
-		SpectroBeamComp(Utils& u, SpecBeam& _beam) :
-			Comp(u, "Spectro Beam", CursorType::Default),
-			mainColCID(ColourID::Hover),
-			xen(u.audioProcessor.xenManager),
-			beam(_beam),
-			img(Image::RGB, Size, 1, true)
-		{
-			setInterceptsMouseClicks(false, false);
-			startTimerHz(60);
-			setOpaque(true);
-		}
+		SpectroBeamComp(Utils&, SpecBeam&);
 
-		void paint(Graphics& g) override
-		{
-			g.setImageResamplingQuality(Graphics::lowResamplingQuality);
-			g.drawImage(img, getLocalBounds().toFloat());
-		}
+		void paint(Graphics&) override;
 
-		void timerCallback() override
-		{
-			auto ready = beam.ready.load();
-			if (!ready)
-				return;
-			
-			const auto Fs = static_cast<float>(utils.audioProcessor.getSampleRate());
-			const auto fsInv = 1.f / Fs;
-			const auto colBase = Colours::c(ColourID::Bg);
-			const auto col = Colours::c(mainColCID);
-			const auto buf = beam.buffer.data();
-
-			const auto lowestDb = -12.f;
-			const auto highestDb = 6.f;
-			const auto rangeDb = highestDb - lowestDb;
-			const auto rangeDbInv = 1.f / rangeDb;
-			
-			for (auto x = 0; x < Size; ++x)
-			{
-				const auto norm = static_cast<float>(x) * SizeInv;
-				const auto pitch = norm * 128.f;
-				const auto freqHz = xen.noteToFreqHzWithWrap(pitch + xen.getXen());
-				const auto binIdx = freqHz * fsInv * SizeF;
-				
-				const auto bin = interpolate::lerp(buf, binIdx);
-				const auto magDb = audio::gainToDecibel(bin);
-				const auto magMapped = juce::jlimit(0.f, 1.f, (magDb - lowestDb) * rangeDbInv);
-				const auto nCol = colBase.interpolatedWith(col, magMapped);
-				img.setPixelAt(x, 0, nCol);
-			}
-			
-			beam.ready.store(false);
-			repaint();
-		}
+		void timerCallback() override;
 		
 		ColourID mainColCID;
 	protected:
