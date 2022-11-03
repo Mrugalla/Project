9be3ca1fc6aeb7d8fe1c70bc3abf9a8373709527
Mrugalla <florian.mrugalla@web.de>
2022-11-03 13:07:31 +0100

    CPPd keyboardComp

--- Source/gui/KeyboardComp.h
+++ Source/gui/KeyboardComp.h
@@ -10,44 +10,16 @@ namespace gui
 		/* noteVal */
 		using PitchCallback = std::function<void(int)>;
 
-	private:
+	protected:
 		struct Key :
 			public Comp
 		{
-			Key(Utils& u, int _noteVal) :
-				Comp(u, "", CursorType::Default),
-				noteVal(_noteVal),
-				bgCol(),
-				label(u, noteVal % 12 == 0 ? "C" + String(noteVal / 12 - 1) : String(""))
-			{
-				const auto whiteKey = audio::isWhiteKey(noteVal % 12);
-				bgCol = (whiteKey ? Colour(0xffffffff) : Colour(0xff000000)).withAlpha(.3f);
-
-				setInterceptsMouseClicks(false, false);
-				
-				if (label.getText().isNotEmpty())
-				{
-					addAndMakeVisible(label);
-					label.textCID = ColourID::Interact;
-				}
-					
-			}
+			/* utils, noteVal */
+			Key(Utils&, int);
 			
-			void paint(Graphics& g) override
-			{
-				const auto thicc = getUtils().thicc;
-				const auto bounds = getLocalBounds().toFloat().reduced(thicc);
-				
-				g.setColour(bgCol);
-				g.fillRoundedRectangle(bounds, thicc);
-			}
+			void paint(Graphics&) override;
 
-			void resized() override
-			{
-				const auto thicc = getUtils().thicc;
-				const auto bounds = getLocalBounds().toFloat().reduced(thicc);
-				label.setBounds(bounds.toNearestInt());
-			}
+			void resized() override;
 			
 			int noteVal;
 		protected:
@@ -56,150 +28,22 @@ namespace gui
 		};
 
 	public:
-		KeyboardComp(Utils& u, String&& _tooltip) :
-			Comp(u, _tooltip, CursorType::Default),
-			onDown([](int) {}),
-			onDrag([](int) {}),
-			onUp([](int) {}),
-			
-			keys(),
-			octDown(u, "Press this button to go down an octave."),
-			octUp(u, "Press this button to go up an octave."),
-			hoverIdx(-1),
-			octIdx(3)
-		{
-			setInterceptsMouseClicks(true, false);
-			
-			layout.init
-			(
-				{ 1, 21, 1 },
-				{ 1 }
-			);
+		/* utils, tooltip */
+		KeyboardComp(Utils&, String&&);
 
-			makeTextButton(octDown, "<<", false);
-			makeTextButton(octUp, ">>", false);
+		void resized() override;
 
-			octDown.onClick.push_back([&](Button&, const Mouse&)
-				{
-					if (octIdx > 0)
-					{
-						--octIdx;
-						keyRangeChanged();
-						resized();
-					}
-				});
-			octUp.onClick.push_back([&](Button&, const Mouse&)
-				{
-					if (octIdx < 8)
-					{
-						++octIdx;
-						keyRangeChanged();
-						resized();
-					}
-				});
-			
-			addAndMakeVisible(octDown);
-			addAndMakeVisible(octUp);
+		void paint(Graphics&) override;
 
-			keyRangeChanged();
-		}
+		void mouseMove(const Mouse&) override;
 
-		void resized() override
-		{
-			layout.resized();
-			
-			layout.place(octDown, 0, 0, 1, 1, false);
-			layout.place(octUp, 2, 0, 1, 1, false);
+		void mouseDown(const Mouse&) override;
 
-			const auto bounds = layout(1, 0, 1, 1);
-			const auto numKeys = keys.size();
-			const auto numKeysInv = 1.f / static_cast<float>(numKeys);
-			const auto y = bounds.getY();
-			const auto w = bounds.getWidth() * numKeysInv;
-			const auto h = bounds.getHeight();
-			for (auto i = 0; i < numKeys; ++i)
-			{
-				const auto iF = static_cast<float>(i);
-				const auto x = bounds.getX() + iF * w;
+		void mouseDrag(const Mouse&) override;
 
-				const BoundsF keyBounds(x, y, w, h);
-				auto& key = keys[i];
-				key->setBounds(keyBounds.toNearestInt());
-				key->setVisible(true);
-			}
-		}
+		void mouseUp(const Mouse&) override;
 
-		void paint(Graphics& g) override
-		{
-			if (hoverIdx != -1)
-			{
-				const auto thicc = getUtils().thicc;
-				const auto bounds = keys[hoverIdx]->getBounds().toFloat().reduced(thicc);
-				g.setColour(Colours::c(ColourID::Hover).withMultipliedAlpha(2.f));
-				g.fillRoundedRectangle(bounds, thicc);
-			}
-		}
-
-		void mouseMove(const Mouse& evt) override
-		{
-			Comp::mouseMove(evt);
-
-			const auto nHoverIdx = getHoverIdx(evt.getPosition());
-			if (hoverIdx != nHoverIdx)
-			{
-				hoverIdx = nHoverIdx;
-				repaint();
-			}
-			
-		}
-
-		void mouseDown(const Mouse& evt) override
-		{
-			Comp::mouseDown(evt);
-
-			hoverIdx = getHoverIdx(evt.getPosition());
-			const auto noteVal = getPitch();
-			onDown(noteVal);
-			repaint();
-		}
-
-		void mouseDrag(const Mouse& evt) override
-		{
-			Comp::mouseDrag(evt);
-
-			const auto pt = evt.getPosition();
-
-			auto nHoverIdx = getHoverIdx(pt);
-			if (nHoverIdx == -1)
-			{
-				if (pt.x < keys[0]->getX())
-					nHoverIdx = 0;
-				else
-					nHoverIdx = static_cast<int>(keys.size() - 1);
-			}
-			if (hoverIdx != nHoverIdx)
-			{
-				hoverIdx = nHoverIdx;
-				const auto noteVal = getPitch();
-				onDrag(noteVal);
-				repaint();
-			}
-		}
-
-		void mouseUp(const Mouse& evt) override
-		{
-			Comp::mouseUp(evt);
-
-			const auto noteVal = getPitch();
-			onUp(noteVal);
-			repaint();
-		}
-
-		void mouseExit(const Mouse&) override
-		{
-			hoverIdx = -1;
-			repaint();
-		}
+		void mouseExit(const Mouse&) override;
 
 		PitchCallback onDown, onDrag, onUp;
 	protected:
@@ -207,36 +51,11 @@ namespace gui
 		Button octDown, octUp;
 		int hoverIdx, octIdx;
 		
-		int getHoverIdx(Point pt) noexcept
-		{
-			for (auto i = 0; i < keys.size(); ++i)
-			{
-				auto& key = keys[i];
-				const auto keyX = key->getX();
-				const auto keyRight = key->getRight();
-				if (pt.x >= keyX && pt.x <= keyRight)
-					return i;
-			}
-			return -1;
-		}
+		int getHoverIdx(Point) noexcept;
 
-		void keyRangeChanged()
-		{
-			for (auto& key : keys)
-				removeChildComponent(key.get());
-
-			for (auto i = 0; i < keys.size(); ++i)
-			{
-				auto& key = keys[i];
-				key = std::make_unique<Key>(utils, i + octIdx * 12);
-				addAndMakeVisible(*key);
-			}
-		}
+		void keyRangeChanged();
 		
-		int getPitch() noexcept
-		{
-			return juce::jlimit(0, 127, hoverIdx + octIdx * 12);
-		}
+		int getPitch() noexcept;
 	};
 	
 }
--- Source/gui/Menu.h
+++ Source/gui/Menu.h
@@ -5,145 +5,6 @@ namespace gui
 {
 	Just getJust(const String&);
 
-	// not used yet:
-	struct ColourShifter :
-		public Comp
-	{
-		enum
-		{
-			Display,
-			Hue,
-			Sat,
-			Bright,
-			NumControls
-		};
-
-		ColourShifter(Utils& u) :
-			Comp(u, "Click and/or drag on this component to select a colour."),
-			bounds(),
-			label
-			{
-				Label(u, "Display"),
-				Label(u, "Hue"),
-				Label(u, "Saturation"),
-				Label(u, "Brightness")
-			},
-			currentColour(0xffff0000)
-		{
-			layout.init
-			(
-				{ 8, 1 },
-				{ 1, 1, 1, 1 }
-			);
-		}
-
-		void setCurrentColour(Colour c)
-		{
-			currentColour = c;
-			repaint();
-		}
-
-		void paint(Graphics& g) override
-		{
-			const auto thicc = utils.thicc;
-			g.fillRoundedRectangle(label[Display].getBounds().toFloat(), thicc);
-
-			float hsb[4]
-			{
-				0.f,
-				currentColour.getHue(),
-				currentColour.getSaturation(),
-				currentColour.getBrightness()
-			};
-
-			{
-				const auto w = static_cast<float>(bounds[Hue].getWidth());
-				const auto h = static_cast<float>(bounds[Hue].getHeight());
-
-				auto hue = 0.f;
-				const auto inc = 1.f / w;
-
-				for (auto x = 0.f; x < w; ++x, hue += inc)
-				{
-					const auto col = Colour::fromHSL(hue, hsb[Sat], hsb[Bright], 1.f);
-					g.setColour(col);
-					g.drawRect(x, 0.f, 1.f, h);
-				}
-
-				g.setColour(Colour(0xff000000));
-				const auto x = inc * hsb[Hue];
-				g.drawRect(x - 1.f, 0.f, 3.f, h);
-			}
-			{
-				const auto w = static_cast<float>(bounds[Sat].getWidth());
-				const auto h = static_cast<float>(bounds[Sat].getHeight());
-
-				auto sat = 0.f;
-				const auto inc = 1.f / w;
-
-				for (auto x = 0.f; x < w; ++x, sat += inc)
-				{
-					const auto col = Colour::fromHSL(hsb[Hue], sat, hsb[Bright], 1.f);
-					g.setColour(col);
-					g.drawRect(x, 0.f, 1.f, h);
-				}
-
-				g.setColour(Colour(0xff000000));
-				const auto x = inc * hsb[Sat];
-				g.drawRect(x - 1.f, 0.f, 3.f, h);
-			}
-			{
-				const auto w = static_cast<float>(bounds[Bright].getWidth());
-				const auto h = static_cast<float>(bounds[Bright].getHeight());
-
-				auto bright = 0.f;
-				const auto inc = 1.f / w;
-
-				for (auto x = 0.f; x < w; ++x, bright += inc)
-				{
-					const auto col = Colour::fromHSL(hsb[Hue], hsb[Sat], bright, 1.f);
-					g.setColour(col);
-					g.drawRect(x, 0.f, 1.f, h);
-				}
-
-				g.setColour(Colour(0xff000000));
-				const auto x = inc * hsb[Bright];
-				g.drawRect(x - 1.f, 0.f, 3.f, h);
-			}
-		}
-
-		void resized() override
-		{
-			layout.resized();
-
-			layout.place(label[Display], 0, 0, 2, 1, false);
-
-			for (auto i = 1; i < NumControls; ++i)
-			{
-				bounds[i] = layout(0, i, 1, 1, false);
-				layout.place(label[i], 1, i, 1, 1, false);
-			}
-		}
-
-		void mouseDown(const Mouse&) override
-		{
-
-		}
-		void mouseDrag(const Mouse&) override
-		{
-
-		}
-		void mouseUp(const Mouse&) override
-		{
-
-		}
-	protected:
-		std::array<BoundsF, NumControls> bounds;
-		std::array<Label, NumControls> label;
-		Colour currentColour;
-	};
-	//
-
 	/// Sub Menus:
 
 	struct ColourSelector :
@@ -300,7 +161,7 @@ namespace gui
 
 	struct ComponentWithBounds
 	{
-		/*comp,bounds,isQuad*/
+		/* comp, bounds, isQuad */
 		template<typename CompType>
 		ComponentWithBounds(CompType* = nullptr, BoundsF&& = BoundsF(0.f, 0.f, 1.f, 1.f), bool = false);
 
@@ -312,7 +173,7 @@ namespace gui
 	struct CompModular :
 		public Comp
 	{
-		/*utils,tooltip,cursorType*/
+		/* utils, tooltip, cursorType */
 		CompModular(Utils&, String&&, CursorType);
 
 		void init();
